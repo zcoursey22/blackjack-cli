913b8a0e84e8c87cb3a52efeff33becf4e2be669
Zach Coursey <zachary.coursey@availity.com>
2019-11-23 17:03:34 -0500

    Adds typewriter effect and changes X to 10

--- blackjack.py
+++ blackjack.py
@@ -5,20 +5,37 @@ import time
 import shelve
 from getch import getch
 from colorama import Fore, Back, Style
+import math
 
 # Dealer will stand on all 17s
 # 2:1 payout for blackjack
 
 # TODO
-# Add typewriter effect
-# Animate card flip
 # Add time delay to drawing
+# Animate card flip
 # Improve selction/input with library
 # Rework UX of exiting
 # Split
 # Surrender
 # Insurance
 # Settings menu for changing rules, etc.
+
+CARD_BACK_COLOR = Back.GREEN
+TYPEWRITE_EFFECT = True
+TYPEWRITE_MIN_SPEED = 0.01
+TYPEWRITE_SPEED = 15
+
+
+def typewrite(s, end='\n'):
+    if TYPEWRITE_EFFECT:
+        n = len(s)
+        for c in s:
+            time.sleep(min(TYPEWRITE_MIN_SPEED,
+                           math.log(n) / n / TYPEWRITE_SPEED))
+            print(c, end='', flush=True)
+        print(end, end='')
+    else:
+        print(s, end=end)
 
 
 class Suit:
@@ -53,7 +70,7 @@ class Deck:
         ]
         self.suits = suits
         self.cards = [Card(rank, suit)
-                      for rank in ['A', 2, 3, 4, 5, 6, 7, 8, 9, 'X', 'J', 'Q', 'K'] for suit in suits]
+                      for rank in ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'] for suit in suits]
         self.used_cards = []
         self.shuffle()
 
@@ -105,17 +122,19 @@ class Dealer:
 
     def show(self, init=False, win=False):
         if not init:
-            print(' '.join([f'{card.suit.color}{Back.WHITE}{card.rank}  {Style.RESET_ALL}' for card in self.hand]
+            print(' '.join([f'{card.suit.color}{Back.WHITE}{f"10 " if card.rank == 10 else f"{card.rank}  "}{Style.RESET_ALL}' for card in self.hand]
                            ))
             print(' '.join([f'{card.suit.color}{Back.WHITE} {card.suit.symbol} {Style.RESET_ALL}' for card in self.hand]
                            ) + f' = {self.get_hand_sum()}{self.get_soft_sum(True, win)}')
-            print(' '.join([f'{card.suit.color}{Back.WHITE}  {card.rank}{Style.RESET_ALL}' for card in self.hand]
+            print(' '.join([f'{card.suit.color}{Back.WHITE}{f" 10" if card.rank == 10 else f"  {card.rank}"}{Style.RESET_ALL}' for card in self.hand]
                            ))
             return
         card = self.hand[0]
-        print(f'{card.suit.color}{Back.WHITE}{card.rank}  {Style.RESET_ALL} {Fore.BLACK}{Back.GREEN}* *{Style.RESET_ALL}')
-        print(f'{card.suit.color}{Back.WHITE} {card.suit.symbol} {Style.RESET_ALL} {Fore.BLACK}{Back.GREEN} * {Style.RESET_ALL} = {card.value}')
-        print(f'{card.suit.color}{Back.WHITE}  {card.rank}{Style.RESET_ALL} {Fore.BLACK}{Back.GREEN}* *{Style.RESET_ALL}')
+        top_row = f"10 " if card.rank == 10 else f"{card.rank}  "
+        bottom_row = f" 10" if card.rank == 10 else f"  {card.rank}"
+        print(f'{card.suit.color}{Back.WHITE}{top_row}{Style.RESET_ALL} {Fore.BLACK}{CARD_BACK_COLOR}* *{Style.RESET_ALL}')
+        print(f'{card.suit.color}{Back.WHITE} {card.suit.symbol} {Style.RESET_ALL} {Fore.BLACK}{CARD_BACK_COLOR} * {Style.RESET_ALL} = {card.value}')
+        print(f'{card.suit.color}{Back.WHITE}{bottom_row}{Style.RESET_ALL} {Fore.BLACK}{CARD_BACK_COLOR}* *{Style.RESET_ALL}')
 
     def hasBlackjack(self):
         return self.get_hand_sum() == 21
@@ -171,11 +190,11 @@ class Player:
 
     def show(self, win=False):
         print(' '.join(
-            [f'{card.suit.color}{Back.WHITE}{card.rank}  {Style.RESET_ALL}' for card in self.hand]))
+            [f'{card.suit.color}{Back.WHITE}{f"10 " if card.rank == 10 else f"{card.rank}  "}{Style.RESET_ALL}' for card in self.hand]))
         print(' '.join(
             [f'{card.suit.color}{Back.WHITE} {card.suit.symbol} {Style.RESET_ALL}' for card in self.hand]) + f' = {self.get_hand_sum()}{self.get_soft_sum(True, win)}')
         print(' '.join(
-            [f'{card.suit.color}{Back.WHITE}  {card.rank}{Style.RESET_ALL}' for card in self.hand]))
+            [f'{card.suit.color}{Back.WHITE}{f" 10" if card.rank == 10 else f"  {card.rank}"}{Style.RESET_ALL}' for card in self.hand]))
 
     def hasBlackjack(self):
         return self.get_hand_sum() == 21
@@ -184,9 +203,9 @@ class Player:
         bet = 0
         while bet < 1 or bet > self.money:
             os.system('clear')
-            print(f'Money: ${self.money}')
-            print('\n\nEnter your bet:\n')
-            bet = input('> $')
+            typewrite(f'Money: ${self.money}')
+            typewrite('\nEnter your bet:\n> $', end='')
+            bet = input()
             try:
                 bet = int(bet)
             except:
@@ -210,7 +229,7 @@ class Game:
             self.display()
         else:
             print()
-        print('Thanks for playing!')
+        typewrite('Thanks for playing!')
         sys.exit()
 
     def play_game(self):
@@ -232,28 +251,24 @@ class Game:
         self.player.show(win)
         print('\n')
         if not win:
-            print(
+            typewrite(
                 f'Money: ${self.player.money + (self.player.bet if playing else 0)}')
-            print(f'Current bet: ${self.player.bet}\n')
+            typewrite(f'Current bet: ${self.player.bet}\n')
 
     def hit(self):
         self.player.draw(self.deck)
         if self.player.hasBlackjack():
             self.winner = self.player
-            print('\nYou have blackjack!')
         elif self.player.get_hand_sum() > 21:
             self.winner = self.dealer
-            print('\nYou busted!')
 
     def stand(self):
         while self.dealer.get_hand_sum() < 17:
             self.dealer.draw(self.deck)
             if self.dealer.hasBlackjack():
                 self.winner = self.dealer
-                print('\nDealer has blackjack!')
         if self.dealer.get_hand_sum() > 21:
             self.winner = self.player
-            print('\nDealer busted!')
         if not self.winner:
             p_sum = self.player.get_hand_sum()
             d_sum = self.dealer.get_hand_sum()
@@ -289,15 +304,16 @@ class Game:
             if self.winner:
                 break
             self.display(True, playing=True)
-            print(
-                '1) hit\n2) stand\n3) double down\n4) split (disabled)\n5) surrender (disabled)\n\n> ', end='')
+            # typewrite(
+            #     '1) hit\n2) stand\n3) double down\n4) split (disabled)\n5) surrender (disabled)\n\n> ', end='')
+            typewrite(
+                '1) hit\n2) stand\n3) double down\n\n> ', end='')
             user_in = getch()
         if user_in == '1':
             self.hit()
         elif user_in == '2':
             self.stand()
         elif user_in == '3':
-            print('E')
             self.double_down()
 
     def play_round(self):
@@ -308,28 +324,33 @@ class Game:
                 self.player.money += self.player.bet * 2
                 if self.winner.hasBlackjack():
                     self.player.money += self.player.bet
-                    print('You got blackjack!')
-                print('You win!')
+                    typewrite('You got blackjack!')
+                elif self.dealer.get_hand_sum() > 21:
+                    typewrite('Dealer busted!')
+                typewrite('You win!')
             elif self.winner == self.dealer:
                 if self.winner.hasBlackjack():
-                    print('Dealer has blackjack!')
-                print('You lose!')
+                    typewrite('Dealer has blackjack!')
+                elif self.dealer.get_hand_sum() > 21:
+                    typewrite('You busted!')
+                typewrite('You lose!')
             else:
                 self.player.money += self.player.bet
-                print('It\'s a push!')
+                typewrite('It\'s a push!')
             self.player.rounds_played += 1
             with shelve.open('data') as db:
                 db['money'] = self.player.money
                 db['rounds_played'] = self.player.rounds_played
             if self.player.money == 0:
-                print(f'\nOh no! You ran out of money!')
+                typewrite(f'\nOh no! You ran out of money!')
                 self.quit_game()
             else:
-                print(f'\nMoney: ${self.player.money}')
-                print('\n\nPress any key to continue (or "q" to quit)\n\n> ', end='')
+                typewrite(f'\nMoney: ${self.player.money}')
+                typewrite(
+                    '\n\nPress any key to continue (or "q" to quit)\n\n> ', end='')
                 user_in = getch()
                 if user_in == 'q':
-                    print(user_in)
+                    typewrite(user_in)
                     self.quit_game()
 
 
